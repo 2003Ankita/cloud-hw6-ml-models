5eb93c37fe5e2075e13ee1c741888998fa9447b1
ANKITA PATRA <ap178152@gmail.com>
2026-04-02 17:00:35 +0000

    steup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 set -e  # stop on error
 
 PROJECT_ID=$(gcloud config get-value project)
-ZONE="us-central1-a"
+ZONE="us-east1-b"
 VM_NAME="hw6-vm"
 
 echo "Using project: $PROJECT_ID"
@@ -11,7 +11,7 @@ echo "Using project: $PROJECT_ID"
 # Create VM with startup script
 gcloud compute instances create $VM_NAME \
     --zone=$ZONE \
-    --machine-type=e2-medium \
+    --machine-type=e2-micro \
     --image-family=debian-11 \
     --image-project=debian-cloud \
     --metadata-from-file startup-script=startup.sh
