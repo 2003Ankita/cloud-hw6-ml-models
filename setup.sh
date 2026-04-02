a1336a448616b3bfc0626e9e2aed5751f912553a
ANKITA PATRA <ap178152@gmail.com>
2026-04-02 17:28:11 +0000

    resolved conflict

--- setup.sh
+++ setup.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-set -e  # stop on error
+set -e
 
 PROJECT_ID=$(gcloud config get-value project)
-ZONE="us-east1-c"
 VM_NAME="hw6-vm"
 
 echo "Using project: $PROJECT_ID"
 
-# Create VM with startup script
-gcloud compute instances create $VM_NAME \
+ZONES=("us-east1-b" "us-east1-c" "us-east4-a" "us-west1-b")
+
+for ZONE in "${ZONES[@]}"; do
+  echo "Trying zone: $ZONE"
+
+  if gcloud compute instances create $VM_NAME \
     --zone=$ZONE \
     --machine-type=e2-micro \
     --image-family=debian-11 \
     --image-project=debian-cloud \
-    --metadata-from-file startup-script=startup.sh
+    --metadata-from-file startup-script=startup.sh; then
 
-echo "VM created successfully."
+    echo "VM created successfully in $ZONE"
+    exit 0
+  else
+    echo "Failed in $ZONE, trying next..."
+  fi
+done
+
+echo "All zones failed. Try again later."
+exit 1
